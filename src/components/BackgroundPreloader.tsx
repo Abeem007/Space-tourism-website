340eb2a6319b2d65be43e94b71a6a46e5e78012c
Bimbola <toluokunade07@gmail.com>
2025-12-03 13:42:40 +0100

    fixed background image loading lag

--- src/components/BackgroundPreloader.tsx
+++ src/components/BackgroundPreloader.tsx
@@ -1,17 +1,29 @@
 import { useEffect } from "react";
 
-const images = import.meta.glob("/public/assets/**/*.{jpg,png}",{eager:true})
+
 
 const BackgroundPreloader = () => {
   useEffect(() => {
-    
+    const backgrounds = [
+      "/assets/home/background-home-mobile.jpg",
+      "/assets/home/background-home-tablet.jpg",
+      "/assets/home/background-home-desktop.jpg",
+      "/assets/destination/background-destination-mobile.jpg",
+      "/assets/destination/background-destination-tablet.jpg",
+      "/assets/destination/background-destination-desktop.jpg",
+      "/assets/crew/background-crew-tablet.jpg",
+      "/assets/crew/background-crew-desktop.jpg",
+      "/assets/technology/background-technology-mobile.jpg",
+      "/assets/technology/background-technology-tablet.jpg",
+      "/assets/technology/background-technology-desktop.jpg",
+    ];
 
-    Object.values(images).forEach((img: any) => {
-      const preloaded = new Image();
-      preloaded.src = img.default;
+    backgrounds.forEach((src) => {
+      const img = new Image();
+      img.src = src;
+      img.decode?.().catch(() => {}); 
     });
   }, []);
-
   return null; 
 };
 
